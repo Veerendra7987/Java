f47cf571c147383f79c76b2d87773cba2e84bbaa
Veerendra7987 <ashuahirwar512@gmail.com>
2025-09-04 17:17:37 +0530

    Create SingleInheritenceMain.java

---
+++ src/SingleInheritenceMain.java
@@ -0,0 +1,14 @@
+package pkg6pmjava;
+
+public class MainSports {
+    public static void main(String[] args) {
+        
+        cricketer c1 = new cricketer();
+        
+        c1.setsports("Virat Kohli", "Cricket");   
+        c1.setcricketer(5, 120);                  
+        
+        c1.getsports();      
+        c1.getcricketer();  
+    }
+}
