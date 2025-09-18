33b048443ddd0754de46200171b51691359dd3b5
Veerendra7987 <ashuahirwar512@gmail.com>
2025-09-18 18:43:54 +0530

    Create StudentMain.java

---
+++ src/StudentMain.java
@@ -0,0 +1,15 @@
+package pkg6pmjava;
+
+public class Newmain {
+
+    public static void main(String[] args) {
+        
+       New1 n1=new New1(); //Create a object for Class
+       n1.sname="ashu";
+       n1.gender="Male";
+       n1.mobileno=793233223;
+       n1.getnew();
+       
+    }
+    
+}
