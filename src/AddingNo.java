be0beea1a6366c5f3157733c01e6699163336594
Veerendra7987 <ashuahirwar512@gmail.com>
2025-09-19 17:46:21 +0530

    Create AddingNo.java

---
+++ src/AddingNo.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Add {
+    int a;
+    int b;
+    int c;
+
+    void getInput() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        b = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        c = sc.nextInt();
+    }
+
+    int sum() {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        Add obj = new Add();
+        obj.getInput();  
+        System.out.println("Sum = " + obj.sum());
+    }
+}
+
