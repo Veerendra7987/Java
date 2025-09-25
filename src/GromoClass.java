664eeae4759d4455b0c69e24d38533230c6e4479
Veerendra7987 <ashuahirwar512@gmail.com>
2025-09-25 09:04:45 +0530

    Create GromoClass.java

---
+++ src/GromoClass.java
@@ -0,0 +1,24 @@
+
+package pkg6pmjava;
+
+public class gromo {
+    
+    int no;
+    String name,addresh;
+    
+    void setgromo(int no,String name,String addresh)
+    {
+        this.no=no;
+        this.name=name;
+        this.addresh=addresh;
+    }
+    
+    void getgromo()
+    {
+        System.out.println("Enter The Number : "+no);
+//        System.out.println(+no);
+        System.out.println("Enter The Name : "+name);
+        System.out.println("Enter The Addresh : "+addresh);
+
+    }       
+}
