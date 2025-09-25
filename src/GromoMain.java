fc5cf3b5eb30403c1d275a55b9338e766a813ef4
Veerendra7987 <ashuahirwar512@gmail.com>
2025-09-25 09:06:35 +0530

    Create GromoMain.java

---
+++ src/GromoMain.java
@@ -0,0 +1,15 @@
+
+package pkg6pmjava;
+
+public class gromomain {
+
+    
+    public static void main(String[] args) {
+        
+        gromo g1=new gromo();
+        g1.setgromo(1, "Ashu Ahirwar", "Makroniya");
+        g1.getgromo();
+
+    }
+    
+}
