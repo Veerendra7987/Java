c406cece7aef8b6f6444e0cfb1de80150af3f0d1
Veerendra7987 <ashuahirwar512@gmail.com>
2025-09-28 21:53:44 +0530

    Create DoWhile.java

---
+++ src/DoWhile.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DoWhileExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number for multiplication table: ");
+        int num = sc.nextInt();
+
+        int i = 1;
+
+
+        do {
+            System.out.println(num + " x " + i + " = " + (num * i));
+            i++;
+        } while (i <= 10);
+
+        sc.close();
+    }
+}
