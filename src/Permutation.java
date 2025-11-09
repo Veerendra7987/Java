c56079bfa06051adb1da5e9c3d33c91d22ea13b3
Veerendra7987 <ashuahirwar512@gmail.com>
2025-11-09 19:22:29 +0530

    Create Permutation.java

---
+++ src/Permutation.java
@@ -0,0 +1,22 @@
+
+
+public class Permutations {
+
+   
+    public static void main(String[] args) {
+       String str="2535";
+       permute(str, "");
+    }
+    public static void permute(String str,String prefix){
+        if(str.length()==0){
+            System.out.println(prefix);
+        }else
+        {
+            for(int i=0;i<str.length();i++){
+            String rem =str.substring(0, i)+str.substring(i+1);
+            permute(rem,prefix + str.charAt(i));
+        }
+        }
+    }
+    
+}
